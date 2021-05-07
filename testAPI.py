5b72a203552aba79836e3f0009955e6857ba6098
Fdelys <frandel932@hotmail.com>
2021-05-07 13:56:03 +0200

    Update testAPI.py
    Push première semaine

--- testAPI.py
+++ testAPI.py
@@ -1,65 +1,46 @@
 #!/usr/bin/python3
 
-import yaml, requests, pprint
+import yaml
+import requests
+import pprint
 
-conf = yaml.safe_load(open('/home/formateur/config.yml', 'r'))
-print(conf)
-conf = conf['poleEmp']
-print('conf:', conf)
+confa = yaml.safe_load(open('/home/formateur/config.yml', 'r'))
+print(confa)
+confb = confa['poleEmp']
+print('conf:', confb)
 
 # On renseigne les variables utilisées pour la requète POST
 URL = 'https://entreprise.pole-emploi.fr/connexion/oauth2/access_token'
 app = 'api_offresdemploiv2 o2dsoffre'
-scope="application_"+conf['IDC']+" "+app
+scope="application_"+confb['IDC']+" "+app
 print('scope: ', scope)
 
 params={"realm":"/partenaire"}
 post_data = {"grant_type": "client_credentials",
-	"client_id": conf['IDC'],
-	"client_secret": conf['CS'],
+	"client_id": confb['IDC'],
+	"client_secret": confb['CS'],
 	"scope": scope}
 headers = {"content-type": "application/x-www-form-urlencoded"}
 
 # Execution de la requète
-req = requests.post(URL, params=params, data=post_data, headers=headers)
-resp = req.json()
-pprint.pprint(resp)
+reqOrigin = requests.post(URL, params=params, data=post_data, headers=headers)
+respOrigin = reqOrigin.json()
+pprint.pprint(respOrigin)
 # Le token !!!
-token = resp['access_token']
+token = respOrigin['access_token']
 print("access token: ", token)
 print("-------------------------------------------------")
 
 # Utilisation du token pour faire une requête anotea
-# https://pole-emploi.io/data/api/anotea?tabgroup-api=documentation&doc-section=api-doc-section-rechercher-les-notes-et-avis-de-formations
 URL = 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search'
-params={"qualification":0,
-        "motsCles":"informatique",
-        "region": "24",
-        "origineOffre":2}
+params={"range":"0-30",
+        "publieeDepuis":31,
+        "codeROME":"K1903"}
 headers = {"Authorization": "Bearer "+token}
-
 req = requests.get(URL, params=params, headers=headers)
 # print(req.content)
+
+rap = req.status_code
+print(rap)
 resp = req.json()
-pprint.pprint(resp)
-
-# for avis in resp['avis']:
-#     print(avis['id'], avis['date'])
-
-# #~ pprint.pprint(resp)
-
-# print("-------------------------------------------------")
-
-# # https://pole-emploi.io/data/api/evenements-pole-emploi
-# URL = 'https://api.emploi-store.fr/partenaire/evenements/v1/salonsenligne'
-# params={}
-# headers = {"Authorization": "Bearer "+token,
-#     "Accept":"application/json"}
-
-# req = requests.get(URL, params=params, headers=headers)
-# #~ print(req.content)
-# resp = req.json()
-
-# #~ pprint.pprint(resp)
-# for evt in resp:
-#     print(evt['titre'], evt['localisation'])
+# pprint.pprint(resp)
