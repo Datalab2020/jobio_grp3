b7420118d3a141f7c7869350bb1ba4176a5d9bfd
Ga-ijin <sarah.cantonnet@gmail.com>
2021-05-24 21:35:11 +0200

    graphs

--- graphs_jobs.R
+++ graphs_jobs.R
@@ -3,7 +3,7 @@ library(tidyverse)
 library(yaml) 
 library(ggplot2)
 
-setwd("/home/formateur/Documents/GitHub/jobio_grp3")
+setwd("C:/Users/Ga/Documents/GitHub/jobio_grp3")
 yml <- yaml.load_file("config.yml")
 
 user = yml$poleEmp$user
@@ -18,20 +18,8 @@ contrat = myco$aggregate('[{"$project": {"_id" :0, "natureContrat": 1} }]')
 #group by natureContrat
 contrat2 = myco$aggregate('[{"$project": {"_id" :0, "natureContrat": 1}},{"$group" : {"_id":"$natureContrat", "nbContrats" : {"$sum" : 1}}}]')
 
-barplot(contrat2, width = "natureContrat", height = "nbContrats",
-        col ="lightblue", main ="Most frequent words",
-        ylab = "Word frequencies")
+names(contrat2)[1] = "nomContrat"
 
-p<-ggplot(data=contrat2, aes(x="_id", y=nbContrats)) +
-  geom_bar(stat = "identity")
-p
-
-ggplot(contrat2) + geom_bar(aes(x = "nbContrats"), fill = "darkblue", width = .5)
-ggplot(rp) + geom_bar(aes(x = departement), fill = "darkblue", width = .5)
-ggplot(contrat2) + geom_bar(aes(x = "_id", y = nbContrats), stat = "identity")
-
-x = ggplot(contrat2, aes(x="_id", y=nbContrats)) + 
+p<-ggplot(data=contrat2, aes(x=nomContrat, y=nbContrats)) +
   geom_bar(stat = "identity", width = 0.5)
-x
-
-ggplot(contrat2) + geom_histogram(aes(x = nbContrats))
+p
