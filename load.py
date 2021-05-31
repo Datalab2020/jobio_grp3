ddd5b7949a6e309e0d8745377024c3e4b86ece59
evecoq <evelina.coq@gmail.com>
2021-05-31 03:18:14 +0200

    Signed-off-by: evecoq <evelina.coq@gmail.com>

--- load.py
+++ load.py
@@ -5,19 +5,20 @@ import yaml, requests, pprint, json
 from pymongo import MongoClient
 import urllib.parse
 from flatten import flatten_json
+import datetime as dt
 import time
 from requests.exceptions import ConnectionError
+import urllib.error
 
 confAll = yaml.safe_load(open('config.yaml', 'r'))
-print(confAll)
+# print(confAll)
 
 conf = confAll['mongo']
 mdp = urllib.parse.quote_plus(conf["password"])
-print(mdp)
 auth = 'mongodb://%s:%s@%s/?authSource=%s' % (conf['username'], mdp, conf['host'], conf['authSource'])
 
-print(auth)
-print("mongo conn:", auth)
+# print(auth)
+# print("mongo conn:", auth)
 client = MongoClient(auth)
 
 db = client['bdd_ecoquelet']
@@ -25,14 +26,17 @@ collec = db['test_job']
 #~ collec.save({"event": "test"})
 
 conf = confAll['pole_emploi']
-print('conf:', conf)
+# print('conf:', conf)
+
+now = dt.datetime.now()
+print("Il est: ", now)
 
 def get_token():
     # On renseigne les variables utilisées pour la requète POST
     URL = 'https://entreprise.pole-emploi.fr/connexion/oauth2/access_token'
     app = 'api_offresdemploiv2 o2dsoffre'
     scope="application_"+conf['PAR']+" "+app
-    print('scope: ', scope)
+    # print('scope: ', scope)
     
     params={"realm":"/partenaire"}
     post_data = {"grant_type": "client_credentials",
@@ -51,44 +55,9 @@ def get_token():
     print("access token: ", token)
     print("-------------------------------------------------")
     return token
-
-# valide_sec = resp['expires_in']
-
-def get_content():
-    i = 0
-    for code in codeROME:
-            for nb in nbEnrg:
-                    params = {"sort":1, "range": nb, "codeROME": code}
-                    print(params)
-                    
-                    headers = {"Authorization": "Bearer "+token,
-                                "Accept":"application/json"}
-                    
-                    call = requests.get(url, params = params, headers = headers)
-                    print(call)
-                    response = call.content
-                    if response == b'':
-                        print("NO CONTENT")
-                        break
-                    else:
-                        print("Ok")
-                        response = call.content
-                        data = json.loads(response)
-                        # pprint.pprint(data)
-                        for block in data["resultats"]:
-                            call = requests.get("https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/{0}".format(block["id"]), headers=headers)
-                            responseb = call.content
-                            datab = json.loads(responseb)
-                            datab = flatten_json(datab)
-                            collec.update_one({"id": block["id"]}, {"$set":datab}, True)
-                            last_params = params
-                            i = i+1
-                            print("ALL DONE ", i)
-                            return last_params
-                            
-
+                         
 url = 'https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/search'
-codeROME = ["M1805", "M1802", "M1403", "M1808", "M1601", "M1810"]
+codeROME = ["M1805", "M1802", "M1403", "M1808", "K1601", "M1810"]
 nbEnrg = ["0-149", "150-299", "300-449", "450-599", "600-749", "750-899", "900-1049"]
 
 offre = []
@@ -97,10 +66,8 @@ token = get_token()
 i=0
 while True:
     try:
-        last_params = get_content()
         for code in codeROME:
             for nb in nbEnrg:
-                # nb = nbEnrg.pop()
                 params = {"sort":1, "range": nb, "codeROME": code}
                 print(params)
                 
@@ -117,7 +84,6 @@ while True:
                     print("Ok")
                     response = call.content
                     data = json.loads(response)
-                    # pprint.pprint(data)
                     for block in data["resultats"]:
                         call = requests.get("https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/{0}".format(block["id"]), headers=headers)
                         responseb = call.content
@@ -125,14 +91,17 @@ while True:
                         datab = flatten_json(datab)
                         collec.update_one({"id": block["id"]}, {"$set":datab}, True)
                         last_params = params
+                        list_params = list(params.values())
+                        last_codeROME = list_params[2]
                         i = i+1
                         # print("ALL DONE ", i)
+                        
     except ValueError or ConnectionError:
         token = get_token()
+        indexROME = codeROME.index(last_codeROME)
+        del codeROME[:indexROME]
         for code in codeROME:
             for nb in nbEnrg:
-                # nb = nbEnrg.pop()
-                params = last_params
                 params = {"sort":1, "range": nb, "codeROME": code}
                 print(params)
                 
@@ -149,7 +118,6 @@ while True:
                     print("Ok")
                     response = call.content
                     data = json.loads(response)
-                    # pprint.pprint(data)
                     for block in data["resultats"]:
                         call = requests.get("https://api.emploi-store.fr/partenaire/offresdemploi/v2/offres/{0}".format(block["id"]), headers=headers)
                         responseb = call.content
@@ -159,5 +127,8 @@ while True:
                         last_params = params
                         i = i+1
                         # print("ALL DONE ", i)
-        continue 
+                        stop_signal = {"sort":1, "range": "0-149", "codeROME": last_codeROME}
+                        if params == stop_signal:
+                            break
+        continue
         
